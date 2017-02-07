aabef36ac3b85fd000d9f0728e0bc4a748418612
Frank Smieja <frank.smieja@smartatech.com>
2017-02-07 17:34:08 +0000

    edit of test file

--- src/test/java/com/st/parsehtml/ParseHtmlTest.java
+++ src/test/java/com/st/parsehtml/ParseHtmlTest.java
@@ -11,50 +11,50 @@ import org.junit.Test;
 
 public class ParseHtmlTest {
 
-	ParseHtml parse;
+    ParseHtml parse;
 
-	@Before
-	public void setUp() throws Exception {
-		parse = new ParseHtml();
-	}
+    @Before
+    public void setUp() throws Exception {
+        parse = new ParseHtml();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public final void testProcessRepoFile() {
-		final String res = parse
-				.processRepoFile("src/test/resources/testfile.html");
-		assertNotNull(res);
-		System.out.println(res);
-		assertTrue(res.trim().equals("Repo repo: 70 days since last change"));
-	}
+    @Test
+    public final void testProcessRepoFile() {
+        final String res = parse
+                .processRepoFile("src/test/resources/testfile.html");
+        assertNotNull(res);
+        System.out.println(res);
+        assertTrue(res.trim().equals("Repo repo: 70 days since last change"));
+    }
 
-	@Test
-	public final void testProcessHtml13() {
-		final String html = "<div class='list2'><div class='age4'/><div class='age4'/></div>";
-		final String res = parse.processHtml(html);
+    @Test
+    public final void testProcessHtml13() {
+        final String html = "<div class='list2'><div class='age4'/><div class='age4'/></div>";
+        final String res = parse.processHtml(html);
 
-		assertNull(res);
-		// assertTrue(res.equals(""));
-	}
+        assertNull(res);
+        // assertTrue(res.equals(""));
+    }
 
-	@Test
-	public final void testZeroResult() {
+    @Test
+    public final void testZeroResult() {
 
-		final long result = parse.performCalc(5, 6);
-		assertEquals(result, 0);
-	}
+        final long result = parse.performCalc(5, 6);
+        assertEquals(result, 0);
+    }
 
-	@Test
-	public final void testProcessHtml2() {
-		final String html = "<div class='list'><div class='age4'/><div class='age4'/></div>";
-		final String res = parse.processHtml(html);
+    @Test
+    public final void testProcessHtml2() {
+        final String html = "<div class='list'><div class='age4'/><div class='age4'/></div>";
+        final String res = parse.processHtml(html);
 
-		assertNotNull(res);
-		// assertTrue(!res.equals(""));
-		//
-	}
+        assertNotNull(res);
+        // assertTrue(!res.equals(""));
+        //
+    }
 
 }
